3941e0b129db64e8a8cdc48fa0ae0978a4635f0c
Situ William <situ_william@yahoo.com>
2017-11-23 16:07:54 -0500

    added most of the CRUD functions

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,24 +1,45 @@
 class EntriesController < ApplicationController
 	def index
-		# render :index
+		@entries = Entry.all
 	end
 	def show
-		
+		@entry = Entry.find(params[:id])
 	end
 	def new
-		
+		@entry = Entry.new
 	end
 	def create
-		redirect_to entries_url
+		@entry = Entry.new
+		@entry.word = params[:entry][:word]
+		@entry.definition = params[:entry][:definition]
+		@entry.language = params[:entry][:language]
+
+		if @entry.save
+			redirect_to entries_url
+		else
+			redirect_to entries_url
+		end
 	end
 
 	def edit
-		# render :edit
+		@entry = Entry.find(params[:id])
 	end
 	def update
-		redirect_to entry_url(params[:id])
+		@entry = Entry.find(params[:id])
+		@entry.word = params[:entry][:word]
+		@entry.definition = params[:entry][:definition]
+		@entry.language = params[:entry][:language]
+
+		if @entry.save
+			redirect_to entry_url(params[:id])
+		else
+			redirect_to entry_url(params[:id])
+		end
+		
 	end
 	def destroy
-		redirect_to index_url
+		@entry = Entry.find(params[:id])
+		@entry.destroy
+		redirect_to entries_url
 	end
 end
